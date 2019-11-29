8e7421cac5ac2a2039b85d03587d70b769ffc1ca
Michael Yin <layerssss@gmail.com>
2019-11-29 14:14:50 +1300

    db/seeds.rb: implement fake seeds

--- vwork_rails/db/seeds.rb
+++ vwork_rails/db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+customers = 10.times.map do
+  Customer.create!(
+    name: Faker::Name.name,
+    address: Faker::Address.full_address,
+    email: Faker::Internet.email,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+end
+
+workers = 5.times.map do
+  Worker.create!(
+    name: Faker::Name.name,
+    address: Faker::Address.full_address,
+    email: Faker::Internet.email,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+end
+
+50.times.map do
+  Job.create!(
+    customer: [*customers, nil].sample,
+    worker: [workers.sample, nil].sample,
+    template_name: '',
+    state: 'DRAFT',
+    planned_start_time: Faker::Time.forward(5),
+    steps: 3.times.map do |step_index|
+      Step.new(
+        name: "Step #{step_index + 1}",
+        address: Faker::Address.full_address
+      )
+    end,
+    custom_fields: 3.times.map do |custom_field_index|
+      CustomField.new(
+        name: "Custom Field #{custom_field_index + 1}",
+        field_type: %w[Text Number Checkbox].sample,
+        value: Faker::Address.full_address
+      )
+    end
+  )
+end
