f76451aa5d704ab311050657215adee4f2968470
Michael Yin <layerssss@gmail.com>
2019-12-13 13:08:36 +1300

    Add TabsPage.test

---
+++ vwork_react_app/src/controls/TabsPage.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import useParams from "../hooks/useParams";
+
+import TabsPage from "./TabsPage";
+
+jest.mock("../hooks/useParams");
+
+it("renders", () => {
+  useParams.mockReturnValue({
+    tab: "jobs",
+    pathnameForParams: () => "/pathnameForParams",
+  });
+
+  shallow(
+    <TabsPage
+      tabs={[
+        {
+          name: "jobs",
+          title: "Jobs",
+          children: <>Jobs</>,
+        },
+        {
+          name: "settings",
+          title: "Settings",
+          children: <>Settings</>,
+        },
+      ]}
+    />,
+  );
+});
