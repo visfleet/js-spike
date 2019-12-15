4755122cab2d06511aa498b49f85045ceea5127b
Michael Yin <layerssss@gmail.com>
2019-12-16 10:29:38 +1300

    Add ApplicationLoader

---
+++ vwork_react_app/src/loaders/ApplicationLoader.test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import ApplicationLoader from "./ApplicationLoader";
+
+it("renders", () => {
+  shallow(<ApplicationLoader />);
+});
