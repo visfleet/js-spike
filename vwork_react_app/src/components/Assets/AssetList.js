d635672a512a547e025bb358588fa1d5f4becbf4
Michael Yin <layerssss@gmail.com>
2019-12-17 09:41:04 +1300

    Implement AssetList

--- vwork_react_app/src/components/Assets/AssetList.js
+++ vwork_react_app/src/components/Assets/AssetList.js
@@ -1,5 +1,65 @@
 import React from "react";
+import gql from "graphql-tag";
+
+import AppTable from "../../controls/AppTable";
+import useData from "../../hooks/useData";
 
 export default function AssetList() {
-  return <>TODO: AssetList</>;
+  const data = useData(gql`
+    query {
+      assets {
+        id
+        name
+        assetModel
+        customer {
+          id
+          name
+        }
+        jobs {
+          id
+        }
+        createdAt
+      }
+    }
+  `);
+
+  return (
+    <>
+      <AppTable
+        columns={[
+          {
+            title: "ID",
+            span: 1,
+          },
+          {
+            title: "Name",
+          },
+          {
+            title: "Model",
+          },
+          {
+            title: "Customer",
+          },
+          {
+            title: "Jobs",
+            span: 1,
+          },
+          {
+            title: "Created At",
+          },
+        ]}
+        rows={data?.assets.map(asset => ({
+          key: asset.id,
+          values: [
+            asset.id,
+            asset.name,
+            asset.assetModel,
+            asset.customer?.name,
+            asset.jobs.length,
+            asset.createdAt,
+          ],
+        }))}
+      />
+    </>
+  );
 }
