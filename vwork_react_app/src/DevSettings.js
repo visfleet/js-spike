a8eba043e96a431042e08eb61050879b47247a99
Michael Yin <layerssss@gmail.com>
2019-12-17 09:29:55 +1300

    DevSettings: add reset cache button

--- vwork_react_app/src/DevSettings.js
+++ vwork_react_app/src/DevSettings.js
@@ -3,6 +3,7 @@ import gql from "graphql-tag";
 
 import useData from "./hooks/useData";
 import useAction from "./hooks/useAction";
+import apolloClient from "./services/apolloClient";
 
 export default function DevSettings() {
   const data = useData(gql`
@@ -27,34 +28,44 @@ export default function DevSettings() {
         margin: 0,
       }}
     >
-      {data && (
-        <select
-          value={String(data.apiRequestDelay)}
-          onChange={event =>
-            apiRequestDelaySet(
-              {
-                apiRequestDelay: Number.parseInt(event.currentTarget.value),
-              },
-              {
-                refetchQueries: [
-                  {
-                    query: gql`
-                      query {
-                        apiRequestDelay @client
-                      }
-                    `,
-                  },
-                ],
-              },
-            )
-          }
-        >
-          <option value="0">No delay</option>
-          <option value="100">100ms</option>
-          <option value="300">300ms</option>
-          <option value="1000">1000ms</option>
-        </select>
-      )}
+      <select
+        style={{ margin: 0, marginRight: 10 }}
+        value={String(data?.apiRequestDelay)}
+        onChange={event =>
+          apiRequestDelaySet(
+            {
+              apiRequestDelay: Number.parseInt(event.currentTarget.value),
+            },
+            {
+              refetchQueries: [
+                {
+                  query: gql`
+                    query {
+                      apiRequestDelay @client
+                    }
+                  `,
+                },
+              ],
+            },
+          )
+        }
+      >
+        {!data && <option value="">loading...</option>}
+        <option value="0">No delay</option>
+        <option value="100">100ms</option>
+        <option value="300">300ms</option>
+        <option value="1000">1000ms</option>
+        <option value="2000">2000ms</option>
+      </select>
+      <button
+        className="btn btn-danger"
+        onClick={async () => {
+          await apolloClient.resetStore();
+          window.location.reload();
+        }}
+      >
+        Reset Cache
+      </button>
     </div>
   );
 }
