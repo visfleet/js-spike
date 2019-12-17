6379a2b5d2884eb277938a09907a64f951e306f6
Michael Yin <layerssss@gmail.com>
2019-12-17 14:58:47 +1300

    CustomerList: implement pagination

--- vwork_react_app/src/components/Customers/CustomerList.js
+++ vwork_react_app/src/components/Customers/CustomerList.js
@@ -1,48 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import gql from "graphql-tag";
 
 import AppTable from "../../controls/AppTable";
+import Paginator from "../../controls/Paginator";
 import useData from "../../hooks/useData";
 
 export default function CustomerList() {
-  const data = useData(gql`
-    query {
-      customers {
-        id
-        name
-        address
-        jobs {
+  const [page, pageSet] = useState(0);
+  const data = useData(
+    gql`
+      query($page: Int!) {
+        customersPaged(page: $page) {
           id
+          totalPages
+          nodes {
+            id
+            name
+            address
+            jobs {
+              id
+            }
+          }
         }
       }
-    }
-  `);
+    `,
+    { page },
+  );
 
   return (
-    <AppTable
-      columns={[
-        {
-          title: "Name",
-        },
-        {
-          title: "ID",
-          span: 1,
-        },
-        {
-          title: "Address",
-          span: 4,
-        },
-        { title: "Jobs", span: 1 },
-      ]}
-      rows={data?.customers.map(customer => ({
-        key: customer.id,
-        values: [
-          customer.name,
-          customer.id,
-          customer.address,
-          customer.jobs.length,
-        ],
-      }))}
-    />
+    <>
+      <AppTable
+        columns={[
+          {
+            title: "Name",
+          },
+          {
+            title: "ID",
+            span: 1,
+          },
+          {
+            title: "Address",
+            span: 4,
+          },
+          { title: "Jobs", span: 1 },
+        ]}
+        rows={data?.customersPaged.nodes.map(customer => ({
+          key: customer.id,
+          values: [
+            customer.name,
+            customer.id,
+            customer.address,
+            customer.jobs.length,
+          ],
+        }))}
+      />
+      <Paginator
+        page={page}
+        totalPages={data?.customersPaged.totalPages}
+        onPageChange={pageSet}
+      />
+    </>
   );
 }
