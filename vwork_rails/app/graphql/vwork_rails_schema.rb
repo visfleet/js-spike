4fc5f953d6fe44002772e4d67cc4d3887e0f670c
Michael Yin <layerssss@gmail.com>
2020-01-20 10:46:21 +1300

    Add graphql-errors

--- vwork_rails/app/graphql/vwork_rails_schema.rb
+++ vwork_rails/app/graphql/vwork_rails_schema.rb
@@ -2,3 +2,9 @@ class VworkRailsSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 end
+
+GraphQL::Errors.configure(VworkRailsSchema) do
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    GraphQL::ExecutionError.new(exception.message)
+  end
+end
